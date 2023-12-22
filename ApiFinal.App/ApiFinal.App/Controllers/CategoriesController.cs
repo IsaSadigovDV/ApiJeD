db84aeb1bf0d43be06cb863f42aecb779335ef8d
IsaSadigovDV <isans@code.edu.az>
2023-12-22 14:53:43 +0400

    updated

--- ApiFinal.App/ApiFinal.App/Controllers/CategoriesController.cs
+++ ApiFinal.App/ApiFinal.App/Controllers/CategoriesController.cs
@@ -31,10 +31,73 @@ namespace ApiFinal.App.Controllers
 
             if(category == null)
             {
-                return NotFound();  
+                return StatusCode(404);  
             }
 
             return StatusCode(200, category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            if(_context.Categories.Any(x=>x.Name.Trim().ToLower() ==category.Name.ToLower())) 
+            {
+                return StatusCode(404, new { description = $"{category.Name} already exist" });
+            }
+
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+            return StatusCode(201, category);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+
+            Category? category = await _context.Categories.
+                Where(x => x.Id == id).FirstOrDefaultAsync();
+
+            if (category == null)
+            {
+                return StatusCode(404);
+            }
+
+            _context.Remove(category);
+            await _context.SaveChangesAsync();
+            return StatusCode(204);
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Category category)
+        {
+            if(!ModelState.IsValid) 
+            {
+                return BadRequest();
+            }
+
+            if (_context.Categories.Any(x => x.Name.Trim().ToLower() == category.Name.ToLower() && x.Id !=id))
+            {
+                return StatusCode(404, new { description = $"{category.Name} already exist" });
+            }
+
+            Category? updatedcategory = await _context.Categories.
+              Where(x => x.Id == id).FirstOrDefaultAsync();
+
+            if (updatedcategory == null)
+            {
+                return StatusCode(404);
+            }
+
+            updatedcategory.Name = category.Name;
+            updatedcategory.Description = category.Description;
+            await _context.SaveChangesAsync();
+            return StatusCode(204);
         }
     }
 }
--- ApiFinal.App/ApiFinal.App/Migrations/20231220155157_categoryupdated.Designer.cs
+++ ApiFinal.App/ApiFinal.App/Migrations/20231220155157_categoryupdated.Designer.cs
@@ -3,6 +3,7 @@ using ApiFinal.App.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ApiFinal.App.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231220155157_categoryupdated")]
+    partial class categoryupdated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
