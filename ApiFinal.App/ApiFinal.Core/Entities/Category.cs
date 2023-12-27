ac6e10f852086e491a28b3749f6f82bcd0b2bf29
Huseyn Qurbanov <72246234+HuseynagaQurbanov@users.noreply.github.com>
2023-12-27 18:17:06 +0400

    Category and Product Relation added

--- ApiFinal.App/ApiFinal.Core/Entities/Category.cs
+++ ApiFinal.App/ApiFinal.Core/Entities/Category.cs
@@ -6,5 +6,6 @@ namespace ApiFinal.Core.Entities
     public class Category : BaseEntity
     {
         public string Name { get; set; }
+        public List<Product> Products { get; set; }
     }
 }
--- ApiFinal.App/ApiFinal.Data/Migrations/20231227141616_CreatedRelationProductAndCategory.Designer.cs
+++ ApiFinal.App/ApiFinal.Data/Migrations/20231227141616_CreatedRelationProductAndCategory.Designer.cs
@@ -4,6 +4,7 @@ using ApiFinal.Data.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ApiFinal.Data.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231227141616_CreatedRelationProductAndCategory")]
+    partial class CreatedRelationProductAndCategory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
