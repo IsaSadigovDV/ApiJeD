3545164eb61850a5f5450e00a321b8ddee755232
Isa Sadigov <125346149+IsaSadigovDV@users.noreply.github.com>
2024-01-01 22:16:37 +0400

    get category from api to view added

---
+++ ApiFinal.App/ApiFinal.Client/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using ApiFinal.Client.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace ApiFinal.Client.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly string EndoPoint = "https://localhost:44302";
+        public async Task<IActionResult> Index()
+        { 
+            HttpClient httpClient = new HttpClient();
+            GetItems<CategoryGetDto> getItems = new GetItems<CategoryGetDto>();
+            getItems.Items = new List<CategoryGetDto>();
+
+
+            var json = await httpClient.GetStringAsync(EndoPoint+ "/api/Categories");
+
+            getItems = JsonConvert.DeserializeObject<GetItems<CategoryGetDto>>(json);
+
+            return View(getItems.Items);
+        }
+    }
+}
---
+++ ApiFinal.App/ApiFinal.Client/Dtos/CategoryGetDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiFinal.Client.Dtos
+{
+    public class CategoryGetDto
+    {
+        public string Name { get; set; }
+    }
+    public class GetItems<T> 
+    {
+        public List<T> Items { get; set; }
+    }
+
+}
