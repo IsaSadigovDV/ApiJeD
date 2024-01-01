f468c00cf526dd73d7824bc6393b7d0ec83b9212
Isa Sadigov <125346149+IsaSadigovDV@users.noreply.github.com>
2024-01-01 21:08:43 +0400

    apifianl.client solution added to project

---
+++ ApiFinal.App/ApiFinal.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ApiFinal.Client.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
